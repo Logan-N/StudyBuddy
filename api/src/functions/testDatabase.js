a7b9d8d868adc7a2b4b68cfbbcf2bd391e61ee9d
Logan Nygren <112436570+Logan-N@users.noreply.github.com>
2026-07-15 17:47:16 -0700

    Enhance testDatabase function with HTTP trigger info
    Updated the testDatabase function to include HTTP trigger details and comments.

--- api/src/functions/testDatabase.js
+++ api/src/functions/testDatabase.js
@@ -1,10 +1,14 @@
+//Connect to Azure Functions
 const { app } = require("@azure/functions");
+//Connect to Database
 const { getConnection } = require("./database");
-
+//TestDatabase Function with HTTP Trigger
 app.http("testDatabase", {
+    //Uses GET request
     methods: ["GET"],
+    //Doesn't require Azure Authentication
     authLevel: "anonymous",
-
+    //Function
     handler: async () => {
         try {
             await getConnection();
