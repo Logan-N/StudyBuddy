4075fb5c0ba8e8503e3963e1a501c278c5a5738e
drakeHubCode <javier.drake@mail.com>
2026-07-23 16:04:00 -0700

    Update Questions.sql

--- SQL/Questions.sql
+++ SQL/Questions.sql
@@ -1,12 +1,10 @@
 Create Table Questions (
-  QuestionID Int(6) Not Null,
-  QuizID Int(6) Not Null,
+  QuestionID Int Not Null,
+  QuizID Int Not Null,
   QuestionText Varchar(256) Not Null,
   Options Varchar(256) Null,
   CorrectAnswer Varchar(256) Not Null,
   Primary Key (QuestionID),
-  Foreign Key (QuizID),
-    References Quiz(QuizID)
-    ON DELETE Cascade
-    ON UPDATE Cascade
+  Foreign Key (QuizID)
+  References Quiz(QuizID)
 );
